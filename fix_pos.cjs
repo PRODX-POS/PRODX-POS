05c802c173c9a773e2de9501bd2dc51468baece4
PRODX-OWNER <prodx.owner@gmail.com>
2026-09-13 20:04:23 +0700

    chore: remove temporary patch scripts

--- fix_pos.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let s = fs.readFileSync('src/modules/pos/PosScreen.tsx', 'utf8');
-
-s = s.replace(
-  '        {/* Product Catalog Grid Scroll Area */}\n        <div className="flex-1 overflow-y-auto p-3 sm:p-4 lg:p-5 no-scrollbar">',
-  '        {/* Product Catalog Grid Scroll Area */}\n        <div className="flex-1 overflow-y-auto p-3 sm:p-4 lg:p-5 pb-24 lg:pb-5 no-scrollbar">'
-);
-
-fs.writeFileSync('src/modules/pos/PosScreen.tsx', s);
