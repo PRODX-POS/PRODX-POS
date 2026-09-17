3b0e1152aea7ff34940374a4022dd9018e130c36
PRODX-OWNER <prodx.owner@gmail.com>
2026-09-17 19:49:50 +0700

    fix(db): make idempotency fingerprint migration rerunnable

--- db/migrations/0012_m2_idempotency_request_fingerprint.sql
+++ db/migrations/0012_m2_idempotency_request_fingerprint.sql
@@ -7,9 +7,19 @@
 ALTER TABLE prodx_orders
   ADD COLUMN IF NOT EXISTS idempotency_request_fingerprint TEXT;
 
-ALTER TABLE prodx_orders
-  ADD CONSTRAINT prodx_orders_idempotency_fingerprint_valid
-  CHECK (idempotency_request_fingerprint IS NULL OR idempotency_request_fingerprint ~ '^[0-9a-f]{64}$');
+DO $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1
+    FROM pg_constraint
+    WHERE conname = 'prodx_orders_idempotency_fingerprint_valid'
+      AND conrelid = 'prodx_orders'::regclass
+  ) THEN
+    ALTER TABLE prodx_orders
+      ADD CONSTRAINT prodx_orders_idempotency_fingerprint_valid
+      CHECK (idempotency_request_fingerprint IS NULL OR idempotency_request_fingerprint ~ '^[0-9a-f]{64}$');
+  END IF;
+END $$;
 
 INSERT INTO prodx_schema_migrations(version)
 VALUES ('0012_m2_idempotency_request_fingerprint')
